03013939deaea14acc1fae3ebe15719463a4b15a
VGand <andreivv@list.ru>
2016-02-29 21:57:46 +0400

    убрал лишние зависимости

--- springMVC/src/main/java/ru/av/controllers/LoginController.java
+++ springMVC/src/main/java/ru/av/controllers/LoginController.java
@@ -3,13 +3,8 @@ package ru.av.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by Полина on 24.02.2016.
- */
 @Controller
 public class LoginController {
 
